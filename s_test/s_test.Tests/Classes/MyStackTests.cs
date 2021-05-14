39fd0567f7af1e52fa8ad09b9abd49ab61da4d5e
infixman <infixman@gmail.com>
2021-05-14 16:31:19 +0800

    modify, use ExpectedObjects to test

--- s_test/s_test.Tests/Classes/MyStackTests.cs
+++ s_test/s_test.Tests/Classes/MyStackTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using s_test.Calsses;
 
 namespace s_test.Tests.Classes
@@ -11,12 +12,12 @@ namespace s_test.Tests.Classes
         {
             var array = new int[] { 1, 3, 2, 4 };
             var expected = new MyStack(array);
-            var result = new MyStack();
-            result.Push(1);
-            result.Push(3);
-            result.Push(2);
-            result.Push(4);
-            Assert.AreEqual(expected, result);
+            var actual = new MyStack();
+            actual.Push(1);
+            actual.Push(3);
+            actual.Push(2);
+            actual.Push(4);
+            expected.ToExpectedObject().ShouldEqual(actual);
         }
     }
 }

3a6323872cef78463b9d9adb86a40c90219c657b
infixman <infixman@gmail.com>
2021-05-14 15:48:35 +0800

    add project

---
+++ s_test/s_test.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace s_test.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

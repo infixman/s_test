ce4364081770a5e6cab993d985f84ab51af8fa58
infixman <infixman@gmail.com>
2021-05-14 19:44:42 +0800

    add, TwoNumbers class

---
+++ s_test/s_test/Calsses/TwoNumbers.cs
@@ -0,0 +1,22 @@
+﻿namespace s_test.Calsses
+{
+    public class TwoNumbers
+    {
+        int[] NumberArray { get; set; }
+
+        public TwoNumbers()
+        {
+            NumberArray = new int[] { };
+        }
+
+        public TwoNumbers(int[] arr)
+        {
+            NumberArray = arr;
+        }
+
+        public int[] GetTwoNumbers()
+        {
+            return new int[] { };
+        }
+    }
+}

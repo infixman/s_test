c4a5ddb958d15f69fbcf733578f418cc53126eda
infixman <infixman@gmail.com>
2021-05-14 16:19:02 +0800

    add, MyStack

---
+++ s_test/s_test/Calsses/MyStack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace s_test.Calsses
+{
+    public class MyStack
+    {
+        Stack<int> myStack;
+
+        public MyStack()
+        {
+
+        }
+
+        public MyStack(IEnumerable<int> sequence)
+        {
+            myStack = new Stack<int>(sequence);
+        }
+
+        public void Push(int val)
+        { 
+        }
+    }
+}

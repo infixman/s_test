4116140863389be8aae38542cb13d4a3d379a618
infixman <infixman@gmail.com>
2021-05-14 17:12:38 +0800

    add, test of pop

--- s_test/s_test.Tests/Classes/MyStackTests.cs
+++ s_test/s_test.Tests/Classes/MyStackTests.cs
@@ -10,13 +10,22 @@ namespace s_test.Tests.Classes
         [TestMethod]
         public void Veriry_Push()
         {
-            var array = new int[] { 1, 3, 2, 4 };
-            var expected = new MyStack(array);
+            var expected = new MyStack(new int[] { 1, 3, 2, 4 });
             var actual = new MyStack();
             actual.Push(1);
             actual.Push(3);
             actual.Push(2);
             actual.Push(4);
+            expected.ToExpectedObject().ShouldEqual(actual);
+        }
+
+        [TestMethod]
+        public void Veriry_Pop()
+        {
+            var expected = new MyStack(new int[] { 1, 3, 2 });
+            var actual = new MyStack(new int[] { 1, 3, 2, 4, 6 });
+            actual.Pop();
+            actual.Pop();
             expected.ToExpectedObject().ShouldEqual(actual);
         }
     }

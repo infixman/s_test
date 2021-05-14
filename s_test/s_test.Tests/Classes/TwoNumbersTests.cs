5b1e12ddc2493343ea0a2651c00e27f15aca996b
infixman <infixman@gmail.com>
2021-05-14 19:50:45 +0800

    fix, Verify_GetTwoNumbers

--- s_test/s_test.Tests/Classes/TwoNumbersTests.cs
+++ s_test/s_test.Tests/Classes/TwoNumbersTests.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using s_test.Calsses;
 
 namespace s_test.Tests.Classes
 {
@@ -9,7 +11,9 @@ namespace s_test.Tests.Classes
         [TestMethod]
         public void Verify_GetTwoNumbers()
         {
-            
+            var expected = new int[] { 4, 9 };
+            var actual = new TwoNumbers(new int[] { 1, 4, 7, 23, 9 }, 13).GetTwoNumbers();
+            expected.ToExpectedObject().ShouldEqual(actual);
         }
     }
 }

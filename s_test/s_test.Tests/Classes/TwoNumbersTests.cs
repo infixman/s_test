ed4af713591895c50ec9cd2c75ceebbd69b8d115
infixman <infixman@gmail.com>
2021-05-14 19:45:03 +0800

    add, TwoNumbersTests class

---
+++ s_test/s_test.Tests/Classes/TwoNumbersTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace s_test.Tests.Classes
+{
+    [TestClass]
+    public class TwoNumbersTests
+    {
+
+        [TestMethod]
+        public void Verify_GetTwoNumbers()
+        {
+            
+        }
+    }
+}

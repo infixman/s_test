bd884802bbe6cb2d384e4c55d7d84e6b47bf4d73
infixman <infixman@gmail.com>
2021-05-14 16:20:13 +0800

    add, MyStackTest

---
+++ s_test/s_test.Tests/Classes/MyStackTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using s_test.Calsses;
+
+namespace s_test.Tests.Classes
+{
+    [TestClass]
+    public class MyStackTests
+    {
+        [TestMethod]
+        public void Veriry_Push()
+        {
+            var array = new int[] { 1, 3, 2, 4 };
+            var expected = new MyStack(array);
+            var result = new MyStack();
+            result.Push(1);
+            result.Push(3);
+            result.Push(2);
+            result.Push(4);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}

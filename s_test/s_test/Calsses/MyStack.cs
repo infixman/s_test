df578c229e55ec18752bb5efe280db5200068239
infixman <infixman@gmail.com>
2021-05-14 19:34:48 +0800

    fix, getmax

--- s_test/s_test/Calsses/MyStack.cs
+++ s_test/s_test/Calsses/MyStack.cs
@@ -1,29 +1,39 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace s_test.Calsses
 {
     public class MyStack
     {
         public Stack<int> myStack;
+        public Stack<int> maxStack;
 
         public MyStack()
         {
             myStack = new Stack<int>();
+            maxStack = new Stack<int>();
         }
 
         public MyStack(IEnumerable<int> sequence)
         {
             myStack = new Stack<int>(sequence);
+            maxStack = new Stack<int>(sequence.OrderBy(x => x));
         }
 
         public void Push(int val)
         {
             myStack.Push(val);
+            if (maxStack.Count == 0 || maxStack.Peek() < val)
+                maxStack.Push(val);
         }
 
         public void Pop()
         {
+            if (myStack.Peek() == maxStack.Peek())
+                maxStack.Pop();
+
             myStack.Pop();
+
         }
 
         public int Top()
@@ -33,6 +43,7 @@ namespace s_test.Calsses
 
         public int GetMax()
         {
+            return maxStack.Peek();
         }
     }
 }
